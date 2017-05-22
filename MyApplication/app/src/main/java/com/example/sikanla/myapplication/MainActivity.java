7f1446ac57c6a8087f554c4e6d4325192af06cb4
CHANE-LAW Cécile <cecile.chane-law@epfedu.fr>
2017-05-22 16:30:56 +0200

    Aaaa

--- MyApplication/app/src/main/java/com/example/sikanla/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sikanla/myapplication/MainActivity.java
@@ -1,22 +1,96 @@
 package com.example.sikanla.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
+    Intent intent;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Log.e("aaa", "lz,fze");
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instantiateFAB();
 
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+    }
+
+    /*protected void onDestroy(){
+
+        if(intent!=null){
+            intent.stopPreview();
+            intent.setPreviewCallback(null);
+
+            intent.release();
+            camera = null;
+        }
+
+
+    }*/
+
+
+    private void instantiateFAB() {
+        floatingActionButton = new FloatingActionButton(this);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fabBtn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private static final int REQUEST_CAMERA_RESULT = 1;
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_RESULT:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Cannot run application because camera service permission have not been granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 
 }
