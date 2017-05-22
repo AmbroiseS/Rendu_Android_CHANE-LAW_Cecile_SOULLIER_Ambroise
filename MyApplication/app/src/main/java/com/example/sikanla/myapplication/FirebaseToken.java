6846dad59eee3ed8a35a3a19447f4c3ed34c980f
AmbroiseS <amb.soullier@gmail.com>
2017-05-22 17:11:01 +0200

    added firebase

---
+++ MyApplication/app/src/main/java/com/example/sikanla/myapplication/FirebaseToken.java
@@ -0,0 +1,27 @@
+package com.example.sikanla.myapplication;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Sikanla on 22/05/2017.
+ */
+
+public class FirebaseToken extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+       // sendRegistrationToServer(refreshedToken);
+    }
+}
