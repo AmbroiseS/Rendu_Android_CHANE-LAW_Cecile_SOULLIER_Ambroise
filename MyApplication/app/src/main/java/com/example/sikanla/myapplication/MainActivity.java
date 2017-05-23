c27d003baf019394afa7da9a8342f0a8925cbf75
AmbroiseS <amb.soullier@gmail.com>
2017-05-23 09:36:25 +0200

    added fireBase Database//cleaned code

--- MyApplication/app/src/main/java/com/example/sikanla/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sikanla/myapplication/MainActivity.java
@@ -1,28 +1,21 @@
 package com.example.sikanla.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -33,17 +26,12 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton floatingActionButton;
-    Intent intent;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private StorageReference storageRef;
+    private static final int REQUEST_CAMERA_RESULT = 1;
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            Log.e("aaa", "lz,fze");
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
+    private StorageReference storageRef;
+    private DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,30 +39,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         instantiateFAB();
         storageRef = FirebaseStorage.getInstance().getReference();
-
-
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
+        myRef = FirebaseDatabase.getInstance().getReference("urlPictures");
+     //   myRef.setValue("Hello, World!");
 
     }
-
-    /*protected void onDestroy(){
-
-        if(intent!=null){
-            intent.stopPreview();
-            intent.setPreviewCallback(null);
-
-            intent.release();
-            camera = null;
-        }
-
-
-    }*/
 
 
     private void instantiateFAB() {
@@ -88,26 +56,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private static final int REQUEST_CAMERA_RESULT = 1;
-
-
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CAMERA_RESULT:
-                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Cannot run application because camera service permission have not been granted", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                break;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
-  private   void sendMethod(){
+    private void sendMethod() {
 
         Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
         StorageReference riversRef = storageRef.child("images/rivers.jpg");
@@ -117,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         // Get a URL to the uploaded content
-                     //   Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        //   Uri downloadUrl = taskSnapshot.getDownloadUrl();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -129,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    void downloadMethod(){
+    void downloadMethod() {
         File localFile = null;
         try {
             localFile = File.createTempFile("images", "jpg");
